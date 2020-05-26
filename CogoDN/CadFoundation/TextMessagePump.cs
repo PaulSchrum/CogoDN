440a37f10ba16cc99221bfbd7131e050894e2c58
Paul Schrum <paul.schrum@gmail.com>
2020-05-25 23:55:54 -0400

    Added class like IObservable, but I can understand how to make one.

---
+++ CogoDN/CadFoundation/TextMessagePump.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadFoundation
+{
+    public class TextMessagePump
+    {
+        private HashSet<IObserver<string>> observers = new HashSet<IObserver<string>>();
+
+        public void Register(IObserver<string> observer, String initMessage = null)
+        { 
+            observers.Add(observer);
+            if(null != initMessage)
+                observer.OnNext(initMessage);
+        }
+
+        public void Unregister(IObserver<string> observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void BroadcastMessage(string message)
+        {
+            if (observers.Count == 0)
+            {
+                Console.WriteLine(message);
+                return;
+            }
+
+            foreach (var observer in observers)
+                observer.OnNext(message);
+        }
+
+    }
+}
