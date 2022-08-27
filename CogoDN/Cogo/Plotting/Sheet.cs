49de4a33eac019d8e41e098c40c70d776c4ceda4
Paul Schrum <paul.schrum@gmail.com>
2022-08-26 22:28:10 -0400

    WIP development.

--- CogoDN/Cogo/Plotting/Sheet.cs
+++ CogoDN/Cogo/Plotting/Sheet.cs
@@ -1,5 +1,8 @@
 ﻿using CadFoundation.Coordinates;
 using Cogo.Plotting.Details;
+using PdfSharpCore;
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,7 +11,27 @@ namespace Cogo.Plotting
 {
     public abstract class Sheet : BoundingBox
     {
+        public PdfDocument document { get; private set; }
+        public PdfPage page { get; private set; }
+        public XGraphics gfx { get; private set; }
+        public XFont font { get; private set; }
+        public SheetPanel sheetPanel { get; private set; }
+
         protected Margins Margins { get; set; }
+        public virtual DecimalUnits height {get; set; }
+        public virtual DecimalUnits width { get; set; }
+        public PlotScale scale { get; set; }
         //protected PlotArea PlotArea { get; private set; }
-    }
+
+        public Sheet()
+        {
+            document = new PdfDocument();
+            PdfPage page = this.document.AddPage();
+            page.Orientation = PageOrientation.Landscape;
+            gfx = XGraphics.FromPdfPage(page);
+            font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+            scale = new PlotScale(new DecimalUnits(1, pUnit.USfoot),
+                new DecimalUnits(1, pUnit.Inch));
+        }
+}
 }
