f20bf1a84dcfc2f01ec69bb7eb6d6d5f02dbf430
Paul Schrum <paul.schrum@gmail.com>
2022-09-28 18:02:44 -0400

    Initial Commit.

---
+++ CogoDN/NonLinearBestFit/CsvManager/GoodFitDataFrame.cs
@@ -0,0 +1,38 @@
+﻿using CadFoundation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NonLinearBestFit.CsvManager
+{
+    public class GoodFitDataFrame : List<GoodFitRecord>
+    {
+        public DirectoryManager fileName { get; private set; }
+
+        private GoodFitDataFrame()
+        {
+
+        }
+
+        public static GoodFitDataFrame Create(string csvFilePath)
+        {
+            var lines = File.ReadAllLines(csvFilePath).ToList();
+            var headerLine = lines[0].Split(",").ToList();
+            GoodFitRecord.hyperbolaColumnIndex = headerLine.FindIndex(a => a.Contains("hyperbolaValue"));
+            GoodFitRecord.cosineColumnIndex = headerLine.FindIndex(a => a.Contains("cosineValue"));
+            GoodFitRecord.rationalColumnIndex = headerLine.FindIndex(a => a.Contains("rationalValue"));
+
+            var newDF = new GoodFitDataFrame();
+            newDF.fileName = DirectoryManager.FromPathString(csvFilePath);
+
+            foreach(var aRow in lines.Skip(1))
+            {
+                newDF.Add(new GoodFitRecord(aRow));
+            }
+
+            return newDF;
+        }
+    }
+}
---
+++ CogoDN/NonLinearBestFit/CsvManager/GoodFitRecord.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonLinearBestFit.CsvManager
+{
+    public class GoodFitRecord
+    {
+        public double station { get; private set; }
+        public double elevation { get; private set; }
+        public double vcLength { get; private set; }
+        public double? hyperbolaValue { get; set; } = null;
+        public double? cosineValue { get; set; } = null;
+        public double? rationalValue { get; set; } = null;
+
+        public static int hyperbolaColumnIndex { get; set; } = -1;
+        public static int cosineColumnIndex { get; set; } = -1;
+        public static int rationalColumnIndex { get; set; } = -1;
+
+        public GoodFitRecord(string inString)
+        {
+            var asList = inString.Split(",");
+            station = Convert.ToDouble(asList[0]);
+            elevation = Convert.ToDouble(asList[1]);
+            vcLength = Convert.ToDouble(asList[2]);
+            if (hyperbolaColumnIndex > -1) hyperbolaValue = Convert.ToDouble(asList[hyperbolaColumnIndex]);
+            if (cosineColumnIndex > -1) cosineValue = Convert.ToDouble(asList[cosineColumnIndex]);
+            if (rationalColumnIndex > -1) rationalValue = Convert.ToDouble(asList[rationalColumnIndex]);
+        }
+
+        public static string headerRow()
+        {
+            var str = new StringBuilder("station,elevation,vcLength");
+            if (hyperbolaColumnIndex > -1)
+                str.Append(",hyperbolaValue");
+            if (cosineColumnIndex > -1)
+                str.Append(",cosineValue");
+            if (rationalColumnIndex > -1)
+                str.Append(",rationalValue");
+            return str.ToString();
+        }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder($"{station},{elevation},{vcLength}");
+            if (hyperbolaColumnIndex > -1)
+                str.Append($",{hyperbolaValue}");
+            if (cosineColumnIndex > -1)
+                str.Append($",{cosineValue}");
+            if (rationalColumnIndex > -1)
+                str.Append($",{rationalValue}");
+            return str.ToString();
+        }
+    }
+}
