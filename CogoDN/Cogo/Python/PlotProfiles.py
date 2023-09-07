0a83159539287d1e359a15ee4236908732f24bf4
Paul Schrum <paul.schrum@gmail.com>
2023-09-07 11:53:22 -0400

    Unknown. Plus added metric adjustment.

--- CogoDN/Cogo/Python/PlotProfiles.py
+++ CogoDN/Cogo/Python/PlotProfiles.py
@@ -10,7 +10,17 @@ from matplotlib import pyplot as plt
 import matplotlib.backends.backend_pdf
 import sys
 from pathlib import Path
-import Pillow
+import PIL
+import  datetime
+from typing import List
+
+plotHyperbolas = True
+original_is_metric = True
+
+def adjustment():
+    if original_is_metric:
+        return 1.0
+    return 0.3048
 
 csv_directory = sys.argv[1]
 
@@ -29,15 +39,43 @@ class paramStuffs:
         with open(file_to_open) as a_file:
             self.all_lines = [line.rstrip() for line in a_file]
 
+    def _split_multiply_merge(self, text_list: List[str], multiplicand: float):
+        ret_list = list()
+        for a_str in text_list:
+            lt, rt = a_str.split('=')
+            if not '%' in a_str:
+                rt = float(rt) * multiplicand
+                a_str = f'{lt} = {rt:.2f}'
+            else:
+                lt = lt.replace('a', '')
+                a_str = f'{lt} = {rt}'
+            ret_list.append(a_str)
+        return ret_list
+
     def get_formatted_left_text(self):
         start_idx = 4
-        return f'{self.all_lines[start_idx]}\n{self.all_lines[start_idx+1]}\n{self.all_lines[start_idx+2]}\n{self.all_lines[start_idx+3]}\n'
+        lines = self.all_lines[start_idx:start_idx+4]
+        lines = self._split_multiply_merge(lines, adjustment())
+
+        return f'{lines[0]}\n{lines[1]}\n{lines[2]}\n{lines[3]}\n'
 
     def get_formatted_right_text(self):
         start_idx = 0
-        return f'{self.all_lines[start_idx]}\n{self.all_lines[start_idx+1]}\n{self.all_lines[start_idx+2]}\n{self.all_lines[start_idx+3]}\n'
+        lines = self.all_lines[start_idx:start_idx+4]
+        lines = self._split_multiply_merge(lines, adjustment())
 
+        return f'{lines[0]}\n{lines[1]}\n{lines[2]}\n{lines[3]}\n'
 
+# from https://gist.githubusercontent.com/thriveth/8560036/raw/3039007a6a1ce06325a62de108a2b2351d2eab6d/CBcolors.py
+color_blind_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+                  '#f781bf', '#a65628', '#984ea3',
+                  '#999999', '#e41a1c', '#dede00']
+# 0 blue; 1 orange; 2 olive green; 3 purple pink;; 4 deep red; 5 purple; 6 gray; 7 matte red
+
+existing_ground_color = color_blind_cycle[6]
+good_fit_hyperbola_color = color_blind_cycle[4]
+
+# outfile = str(pdf_path.resolve()) + '\\' + 'hyperbola plots original ground.pdf'
 outfile = str(pdf_path.resolve()) + '\\' + 'hyperbola plots.pdf'
 pdf = matplotlib.backends.backend_pdf.PdfPages(outfile)
 for a_file in csv_files:
@@ -47,8 +85,8 @@ for a_file in csv_files:
     plt.rcParams["figure.figsize"] = [7.00, 3.50]
     plt.rcParams["figure.autolayout"] = True
     fig, ax = plt.subplots(figsize=(7.0, 3.5))
-    ax.set_ylabel('Elevation')
-    ax.set_xlabel('Offset')
+    ax.set_ylabel('Elevation (m)')
+    ax.set_xlabel('Offset (m)')
 
     # Make a list of columns
     # columns = ['mpg', 'displ', 'hp', 'weight']
@@ -56,25 +94,53 @@ for a_file in csv_files:
     with open(a_file) as file_handle:
         columns = file_handle.readline().rstrip().split(",")
     columnsToUse = [x for x in columns if x != "vcLength"]
+    if plotHyperbolas == True:
+        columnsToUse = ['station', 'elevation', 'hyperbolaValue']
+    else:
+        columnsToUse = ['station', 'elevation'] #, 'hyperbolaValue']
 
     # Read a CSV file
     df = pd.read_csv(a_file, header=0, names=columns, usecols=columnsToUse, index_col='station')
+    df.index *= adjustment()
+    df['elevation'] *= adjustment()
+    if plotHyperbolas == True:
+        df['hyperbolaValue'] *= adjustment()
 
     # Plot the lines
     df_elevation = df["elevation"]
-    df_elevation.plot.line(linewidth=2, color="dimgrey", linestyle='dashed', y=["elevation"])
-    df_hyperbola = df["hyperbolaValue"]
-    df_hyperbola.plot.line(linewidth=1, color="red", linestyle='solid')
+    df_elevation.plot.line(linewidth=2, color=existing_ground_color, linestyle='dashed', y=["elevation"])
+    if plotHyperbolas == True:
+        df_hyperbola = df["hyperbolaValue"]
+        df_hyperbola.plot.line(linewidth=1, color=good_fit_hyperbola_color, linestyle='solid')
 
+    plt.xlabel("Offset (m)")
     plt.legend()
     title = Path(a_file).stem[:-4]
     plt.title(title)
+    plt.rcParams['axes.facecolor'] = 'white'
+    plt.grid(visible=True, which='major', color='k', linestyle='-', zorder=2)
+    plt.grid(visible=True, which='minor', color='silver', linestyle='-', linewidth=0.2, zorder=1, alpha=0.9)
+    plt.minorticks_on()
+    plt.subplots_adjust(bottom=-4.35)
     left_text = params.get_formatted_left_text()
     right_text = params.get_formatted_right_text()
-    plt.figtext(0.1, 0.01, left_text, horizontalalignment='left', fontsize=12)
-    plt.figtext(0.7, 0.01, right_text, horizontalalignment='left', fontsize=12)
+    plt.figtext(0.1, 0.01, left_text, horizontalalignment='left',
+                verticalalignment='bottom', fontsize=12)
+    plt.figtext(0.7, 0.01, right_text, horizontalalignment='left',
+                verticalalignment='bottom', fontsize=12)
+    # ax.patch.set_edgecolor('black')
+    # ax.patch.set_linewidth(5)
+    # ax.patch.set(zorder=0)
 
     plt.gca().set_aspect('equal')
+    fig = matplotlib.pyplot.gcf()
+    scale = 100.0
+    bottomMarg = 1.6
+    pageHeight = fig.bbox.y1 / scale
+    pageHeight += bottomMarg
+    fig.set_size_inches(7.5, pageHeight)
+
+    # fig.set_size_inches(7.5, 10.5)
 
 
     # plot = df.plot(linewidth=2, color="dimgrey", linestyle='dashed', title=a_file.stem)
@@ -95,6 +161,9 @@ for a_file in csv_files:
 
 pdf.close()
 
-
+print(f'created {outfile}')
+print(f'at {datetime.datetime.now().strftime("%H:%M")}')
+print()
+print()
 
 
